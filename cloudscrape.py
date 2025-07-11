6a90f4a70faabf69ee4f599c9ade29de1893a166
tposejank <81495861+tposejank@users.noreply.github.com>
2025-07-10 21:59:15 -0400

    aha fuck you then

--- cloudscrape.py
+++ cloudscrape.py
@@ -13,7 +13,7 @@ def get_next_data(request_text):
 scraper = cloudscraper.create_scraper()
 req = scraper.get('https://mx.radiodisney.com/')
 
-print(req.status, req.reason)
+print(req.status_code, req.reason)
 print(req.text)
 
 extracted_data = get_next_data(req.text)
