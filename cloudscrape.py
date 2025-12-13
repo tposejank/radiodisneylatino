55c17eb4a9ad298cc461a8c512fae64c7c91381d
tposejank <81495861+tposejank@users.noreply.github.com>
2025-12-12 22:37:14 -0500

    skip costa rica

--- cloudscrape.py
+++ cloudscrape.py
@@ -11,6 +11,8 @@ stations_data = json.loads(req.text)
 station_data = {}
 for country in stations_data['stations']:
     code = country['code'].upper()
+    if code == 'CR':
+        continue
     station_data[code] = country['stations']
 
 open('stations.json', 'w').write(json.dumps(station_data, indent=4))
