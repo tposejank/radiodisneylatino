d3a136fe83c501a6642a11956320c49cd56893b0
tposejank <81495861+tposejank@users.noreply.github.com>
2026-02-17 21:48:41 -0500

    this has been anoying me for too long

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  server: {
+    allowedHosts: ["ftracker-ca-on-dev2.festivaltracker.org"],
+  },
 });
