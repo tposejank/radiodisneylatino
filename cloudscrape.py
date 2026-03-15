d43ee744682876d1ff2300c84b17a75119054751
tposejank <81495861+tposejank@users.noreply.github.com>
2026-03-14 22:43:55 -0400

    Remove condition to skip 'CR' station code

--- cloudscrape.py
+++ cloudscrape.py
@@ -11,8 +11,7 @@ stations_data = json.loads(req.text)
 station_data = {}
 for country in stations_data['stations']:
     code = country['code'].upper()
-    if code == 'CR':
-        continue
+    
     station_data[code] = country['stations']
 
 open('stations.json', 'w').write(json.dumps(station_data, indent=4))
